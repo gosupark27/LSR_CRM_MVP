b9183bef41a34ad0b850ea7fcecb78fcf53b2556
gosupark27 <hpark41@wgu.edu>
2026-06-12 21:08:56 -0700

    updated insert queries to not have hardcoded ids, and returns customer and ticket ids and created_at timestamps in response

--- ticket_search.py
+++ ticket_search.py
@@ -13,18 +13,19 @@ def ticket_search(phone):
     if row:
         return row._asdict()
 
-def intake(first_name, last_name, phone, email, customer_id, ticket_id, item_type, service_details, total_bal, status):
-    customer_query = text("INSERT INTO customers (customer_id, first_name, last_name, phone, email) VALUES (:cust_val, :first_val, :last_val, :phone_val, :email_val) RETURNING customer_id, created_at")
-    customer_param = {"cust_val": customer_id, "first_val": first_name, "last_val": last_name, "phone_val": phone, "email_val": email}
-    ticket_query = text("INSERT INTO  tickets (ticket_id, customer_id, item_type, service_details, total_balance, \"status\") VALUES (:ticket_val, :cust_val, :item_val, :service_val, :bal_val, :status_val) RETURNING ticket_id, created_at")
-    ticket_param = {"ticket_val": ticket_id, "cust_val": customer_id, "item_val": item_type, "service_val": service_details, "bal_val": total_bal, "status_val": status}
+def intake(first_name, last_name, phone, email, item_type, service_details, total_bal, status):
+    customer_query = text("INSERT INTO customers (first_name, last_name, phone, email) VALUES (:first_val, :last_val, :phone_val, :email_val) RETURNING customer_id, created_at")
+    customer_param = {"first_val": first_name, "last_val": last_name, "phone_val": phone, "email_val": email}
+    ticket_query = text("INSERT INTO  tickets (customer_id, item_type, service_details, total_balance, \"status\") VALUES (:cust_val, :item_val, :service_val, :bal_val, :status_val) RETURNING ticket_id, created_at")
+    ticket_param = {"item_val": item_type, "service_val": service_details, "bal_val": total_bal, "status_val": status}
 
 
     with db.engine.begin() as connection:
         customer_result = connection.execute(customer_query, customer_param)
-        ticket_result = connection.execute(ticket_query, ticket_param)
-
         customer_data = customer_result.first()
+        
+        ticket_param["cust_val"] = customer_data[0]
+        ticket_result = connection.execute(ticket_query, ticket_param)
         ticket_data = ticket_result.first()
 
     return customer_data, ticket_data
@@ -32,8 +33,6 @@ def intake(first_name, last_name, phone, email, customer_id, ticket_id, item_typ
 @app.route('/v1/tickets', methods=['POST'])
 def main():
     status = 'received'
-    customer_id = 5
-    ticket_id = 1030
 
     payload = request.get_json()
     first_name = payload["first_name"]
@@ -44,17 +43,16 @@ def main():
     svc_detail = payload["svc_detail"]
     tot_bal = payload["tot_bal"]
 
-    customer_row, ticket_row = intake(first_name, last_name, phone, email, customer_id, ticket_id, item_type, svc_detail, tot_bal, status)
-    print(customer_row)
-    print(ticket_row)
+    customer_row, ticket_row = intake(first_name, last_name, phone, email, item_type, svc_detail, tot_bal, status)
+    
     db_data_dict = {
         "customer": {
             "customer_id": customer_row[0],
-            "first_name": customer_row[1],
+            "created_at": customer_row[1],
         },
         "ticket": {
             "ticket_id": ticket_row[0],
-            "customer_id": ticket_row[1],
+            "created_at": ticket_row[1],
         }
     }
     
@@ -66,5 +64,6 @@ def main():
         "item_type": item_type,
         "svc_detail": svc_detail,
         "tot_bal": tot_bal,
-        # "db_data": db_data_dict,
+        "cust_data": db_data_dict["customer"],
+        "ticket_data": db_data_dict["ticket"],
         }), 200
