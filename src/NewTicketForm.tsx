5f7593a68719868a35c78f6789c33b76f2fbc1aa
gosupark27 <hpark41@wgu.edu>
2026-06-23 16:48:42 -0700

    Added appshell for the ideal display layout

--- src/NewTicketForm.tsx
+++ src/NewTicketForm.tsx
@@ -1,38 +1,61 @@
-import { AppShell } from "@mantine/core";
-import NewTicketStepper from "./NewTicketStepper";
 import { useState } from "react";
-import ViewNewTicket from "./ViewNewTicket";
+import { Item, ItemDetails, Repair } from "./types.ts";
+import { Button, AppShell } from '@mantine/core';
+import NewItemForm from "./NewItemForm.tsx";
+import NewRepairForm from "./NewRepairForm.tsx";
+
 
 export default function NewTicketForm() {
-    const [active, setActive] = useState(0);
+    const [itemDetails, setItemDetails] = useState<ItemDetails | null>(null);
+    const [repairs, setRepairs] = useState<Repair[]>([]);
+    const [items, setItems] = useState<Item[]>([]);
 
-    const renderForm = (step : number) => {
-        switch (step) {
-            case 0: return <ItemForm/>
-            case 1: return <RepairForm/>
-            case 2: return <TicketForm/>
-            case 3: return <CustomerForm/>
-            case 4: return <ReviewForm/>
-            case 5: return <PaymentForm/>
-            case 6: return <ViewNewTicket/>
-        }
-    }
+    const handleAddItemToTicket = () => {
+        if (!itemDetails){
+            return;
+        };
+
+        const completedItem : Item = {
+            ...itemDetails,
+            repairs
+        };
+
+        setItems((prev) => [...prev, completedItem]);
+
+        setItemDetails(null);
+        setRepairs([]);
+    };
 
     return (
-        <AppShell
-            padding="md"
-            header={{ height: 60 }}
-        >
-            <AppShell.Header>
-                <NewTicketStepper active={active}/>
-            </AppShell.Header>
-            <AppShell.Main>
-                {renderForm(active)}
-            </AppShell.Main>
-            <AppShell.Aside>
-                <NewTicketCart/>
-            </AppShell.Aside>
+        <>
+            <AppShell
+                padding="md"
+                component="form"
+                header={{ height: 60 }}
+            >
+                <AppShell.Header>
 
-        </AppShell>
+                </AppShell.Header>
+                <AppShell.Main>
+                    <NewItemForm onSaveItemDetails={setItemDetails}/>
+                    <NewRepairForm onSaveRepairValues={setRepairs}/>
+                    <Button onClick={handleAddItemToTicket}>Add item to ticket</Button>
+                </AppShell.Main>
+                <AppShell.Aside>
+                    <div>{items.map((item) => {
+                    return(
+                    <>
+                        <div>{item.item_type}</div>
+                        <div>{item.category}</div>
+                        <div>{item.repairs[0].rp_service}</div>
+                        <div>{item.repairs[0].note}</div>
+                    </>
+                    )
+                    })}</div>
+                </AppShell.Aside>
+            </AppShell>
+            
+            
+        </>
     )
 }
