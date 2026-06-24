702fc22e18668a44bdb98a9bbc4826519d313e8e
gosupark27 <hpark41@wgu.edu>
2026-06-24 14:18:09 -0700

    Wired up CustomerInfoStep

--- src/CustomerInfoStep.tsx
+++ src/CustomerInfoStep.tsx
@@ -1,6 +1,17 @@
-export default function CustomerInfoStep() {
+import NewCustomerForm from "./NewCustomerForm";
+import NewScheduleForm from "./NewScheduleForm";
+import { CustomerInfo, DateInfo } from "./types";
+
+interface CustomerInfoStepProps {
+    onSaveCustomerDetails: (customerDetails : CustomerInfo) => void;
+    onSaveDateDetails: (datDetails : DateInfo) => void;
+}
+
+export default function CustomerInfoStep({onSaveCustomerDetails, onSaveDateDetails} : CustomerInfoStepProps) {
     return(
         <>
+            <NewCustomerForm onSaveCustomerDetails={onSaveCustomerDetails}/>
+            <NewScheduleForm onSaveDateDetails={onSaveDateDetails}/>
         </>
     )
 }
