e73517998dd789fbe7798bf4650107a204ba1da6
gosupark27 <hpark41@wgu.edu>
2026-06-24 14:10:51 -0700

    Set up the NewItemForm and NewRepairForm

--- src/BuildTicketStep.tsx
+++ src/BuildTicketStep.tsx
@@ -1,6 +1,17 @@
-export default function BuildTicketStep() {
+import NewItemForm from "./NewItemForm";
+import NewRepairForm from "./NewRepairForm";
+import { ItemDetails, Repair } from "./types";
+
+interface BuildTicketStepProps {
+    onSaveItemDetails: (itemDetails : ItemDetails) => void;
+    onSaveRepairValues: (newRepair : Repair) => void;
+};
+
+export default function BuildTicketStep({onSaveItemDetails, onSaveRepairValues} : BuildTicketStepProps) {
     return(
         <>
+            <NewItemForm onSaveItemDetails={onSaveItemDetails}/>
+            <NewRepairForm onSaveRepairValues={onSaveRepairValues}/>
         </>
     )
 }
