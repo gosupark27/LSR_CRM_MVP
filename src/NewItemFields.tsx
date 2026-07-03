13f6867f0daf926c310ceae1a2e2b0bc4aff9e87
gosupark27 <hpark41@wgu.edu>
2026-07-03 10:47:52 -0700

    Dropdown with grouping options, correct icons for each category label, and adding item shows up on LiveWorkOrder

--- src/NewItemFields.tsx
+++ src/NewItemFields.tsx
@@ -17,6 +17,7 @@ import {
   SuitcaseRollingIcon,
   BootIcon,
   PuzzlePieceIcon,
+  Icon,
 } from "@phosphor-icons/react";
 
 interface NewItemFieldsProps {
@@ -33,44 +34,82 @@ export default function NewItemFields({
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
   });
-  const bags = [
-    "Tote Bag",
-    "Shoulder Bag",
-    "Satchel",
-    "Crossbody Bag",
-    "Clutch",
-    "Hobo | Bucket Bag",
-    "Travel Bag | Backpack",
+
+  const itemTypes = [
+    { value: "Golf Bag", label: "Golf Bag", category: "others" },
+    { value: "Clothes", label: "Clothes", category: "others" },
+    { value: "Belt", label: "Belt", category: "others" },
+    { value: "Other", label: "Other", category: "others" },
+    {
+      value: "Hardside Suitcase",
+      label: "Hardside Suitcase",
+      category: "luggages",
+    },
+    {
+      value: "Softside Suitcase",
+      label: "Softside Suitcase",
+      category: "luggages",
+    },
+    { value: "Duffles", label: "Duffles", category: "luggages" },
+    { value: "Briefcase", label: "Briefcase", category: "luggages" },
+    {
+      value: "Travel Backpack",
+      label: "Travel Backpack",
+      category: "luggages",
+    },
+    { value: "Dress Shoe", label: "Dress Shoe", category: "mens" },
+    {
+      value: "Loafers | Monk Straps",
+      label: "Loafers | Monk Straps",
+      category: "mens",
+    },
+    { value: "Boots", label: "Boots", category: "mens" },
+    { value: "Sneakers", label: "Sneakers", category: "mens" },
+    {
+      value: "Boat Shoes | Sanadals",
+      label: "Boat Shoes | Sanadals",
+      category: "mens",
+    },
+    { value: "Work Boots", label: "Work Boots", category: "mens" },
+    { value: "Pumps | Heels", label: "Pumps | Heels", category: "womens" },
+    {
+      value: "Flats | Ballerinas",
+      label: "Flats | Ballerinas",
+      category: "womens",
+    },
+    { value: "Boots | Booties", label: "Boots | Booties", category: "womens" },
+    { value: "Loafers | Mules", label: "Loafers | Mules", category: "womens" },
+    { value: "Sandals", label: "Sandals", category: "womens" },
+    { value: "Sneakers", label: "Sneakers", category: "womens" },
+    { value: "Tote Bag", label: "Tote Bag", category: "bags" },
+    { value: "Shoulder Bag", label: "Shoulder Bag", category: "bags" },
+    { value: "Satchel", label: "Satchel", category: "bags" },
+    { value: "Crossbody Bag", label: "Crossbody Bag", category: "bags" },
+    { value: "Clutch", label: "Clutch", category: "bags" },
+    {
+      value: "Hobo | Bucket Bag",
+      label: "Hobo | Bucket Bag",
+      category: "bags",
+    },
+    {
+      value: "Travel Bag | Backpack",
+      label: "Travel Bag | Backpack",
+      category: "bags",
+    },
   ];
-  const womens_shoes = [
-    "Pumps | Heels",
-    "Flats | Ballerinas",
-    "Boots | Booties",
-    "Loafers | Mules",
-    "Sandals",
-    "Sneakers",
-  ];
-  const mens_shoes = [
-    "Dress Shoes",
-    "Loafers | Monk Straps",
-    "Boots",
-    "Sneakers",
-    "Boat Shoes | Sanadals",
-    "Work Boots",
-  ];
-  const luggages = [
-    "Hardside Suitcase",
-    "Softside Suitcase",
-    "Duffles",
-    "Briefcases",
-    "Travel Backpack",
-  ];
-  const others = [
-    'Golf Bag',
-    'Clothes',
-    'Belt',
-    'Other',
-  ]
+  const categoryOrder = ["bags", "womens", "mens", "luggages", "others"];
+  const categoryLabels: Record<string, Icon> = {
+    bags: HandbagIcon,
+    womens: HighHeelIcon,
+    mens: BootIcon,
+    luggages: SuitcaseRollingIcon,
+    others: PuzzlePieceIcon,
+  };
+  const groupedItemTypes = Object.entries(
+    Object.groupBy(itemTypes, (itemType) => itemType.category),
+  ).sort(([categoryA], [categoryB]) => {
+    return categoryOrder.indexOf(categoryA) - categoryOrder.indexOf(categoryB);
+  });
 
   const handleSaveItem = () => {
     const item = form.getValues().draftItem;
@@ -91,8 +130,10 @@ export default function NewItemFields({
       <Group wrap="nowrap" justify="flex-start">
         <Combobox
           store={combobox}
-          onOptionSubmit={(val) => {
-            form.setFieldValue("draftItem.category", val);
+          onOptionSubmit={(val, optionProps) => {
+            const category = (optionProps as any)["data-category"]
+            form.setFieldValue("draftItem.item_type", val);
+            form.setFieldValue("draftItem.category", category);
             combobox.closeDropdown();
           }}
         >
@@ -105,9 +146,9 @@ export default function NewItemFields({
               rightSection={<Combobox.Chevron />}
               rightSectionPointerEvents="none"
               onClick={() => combobox.openDropdown()}
-              key=''
+              key=""
             >
-              {form.values.draftItem.category || (
+              {form.values.draftItem.item_type || (
                 <Input.Placeholder>Select Item</Input.Placeholder>
               )}
             </InputBase>
@@ -115,80 +156,35 @@ export default function NewItemFields({
 
           <Combobox.Dropdown>
             <Combobox.Options>
-              <ScrollArea.Autosize
-                mah={250}
-                type="scroll"
-              >
-                <Combobox.Group
-                  label={
-                    <Group gap="xs" align="center">
-                      <HandbagIcon size={16} style={{ display: "block" }} />
-                      <Text>Bags</Text>
-                    </Group>
-                  }
-                >
-                  {bags.map((bag) => (
-                    <Combobox.Option value={bag} key={bag} ta="center">
-                      {bag}
-                    </Combobox.Option>
-                  ))}
-                </Combobox.Group>
-                <Combobox.Group
-                  label={
-                    <Group gap="xs" align="center">
-                      <HighHeelIcon size={16} style={{ display: "block" }} />
-                      <Text>Women</Text>
-                    </Group>
-                  }
-                >
-                  {womens_shoes.map((shoe) => (
-                    <Combobox.Option value={shoe} key={shoe} ta="center">
-                      {shoe}
-                    </Combobox.Option>
-                  ))}
-                </Combobox.Group>
-                <Combobox.Group
-                  label={
-                    <Group gap="xs" align="center">
-                      <BootIcon size={16} style={{ display: "block" }} />
-                      <Text>Men</Text>
-                    </Group>
-                  }
-                >
-                  {mens_shoes.map((shoe) => (
-                    <Combobox.Option value={shoe} key={shoe} ta="center">
-                      {shoe}
-                    </Combobox.Option>
-                  ))}
-                </Combobox.Group>
-                <Combobox.Group
-                  label={
-                    <Group gap="xs" align="center">
-                      <SuitcaseRollingIcon size={16} style={{ display: "block" }} />
-                      <Text>Luggage</Text>
-                    </Group>
-                  }
-                >
-                  {luggages.map((luggage) => (
-                    <Combobox.Option value={luggage} key={luggage} ta="center">
-                      {luggage}
-                    </Combobox.Option>
-                  ))}
-                </Combobox.Group>
-                <Combobox.Group
-                  label={
-                    <Group gap="xs" align="center">
-                      <PuzzlePieceIcon size={16} style={{ display: "block" }} />
-                      <Text>Other</Text>
-                    </Group>
-                  }
-                >
-                  {others.map((other) => (
-                    <Combobox.Option value={other} key={other} ta="center">
-                      {other}
-                    </Combobox.Option>
-                  ))}
-                </Combobox.Group>
+              <ScrollArea.Autosize mah={250} type="scroll">
+                {groupedItemTypes.map(([category, itemTypes]) => {
+                  const IconComponent = categoryLabels[category];
+                  return (
+                    <Combobox.Group
+                      key={category}
+                      label={
+                        <Group gap="xs" align="center">
+                          <IconComponent
+                            size={16}
+                            style={{ display: "block" }}
+                          />
+                          <Text tt="uppercase">{category}</Text>
+                        </Group>
+                      }
+                    >
+                      {itemTypes?.map((item) => (
+                        <Combobox.Option
+                          value={item.label}
+                          key={item.value}
+                          data-category={category}
+                          ta="center"
+                        >
+                          {item.label}
+                        </Combobox.Option>
+                      ))}
+                    </Combobox.Group>
+                  );
+                })}
               </ScrollArea.Autosize>
             </Combobox.Options>
           </Combobox.Dropdown>
